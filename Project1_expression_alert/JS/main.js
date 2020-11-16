798dfe4d4b0153644268eab334b92ebed727bd78
pierbr03 <72429715+pierbr03@users.noreply.github.com>
2020-11-15 20:35:16 -0800

    Uploaded Assignment

---
+++ Project1_expression_alert/JS/main.js
@@ -0,0 +1,10 @@
+window.alert("Howdy Y\'all"); //Alert box with use of apostrophe 
+
+document.write(4 + 3); //This is the expression I used resulting in 7
+
+var A = "Tennis" + "Is the best sport", B = "Golf"; //Here I have assigned more than one variable. I have also assigned a concatenated string value within variable A
+
+var A = A.fontcolor("red"); //Here I have assigned the variable A to be displayed in red
+document.write(A); //Here I am using document write to disply the text within variable A
+
+
